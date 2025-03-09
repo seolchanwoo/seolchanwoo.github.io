6eff18d849631bd182f39263e8d25546f581ed00
snow <cksdn0716@naver.com>
2025-03-09 15:53:04 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -285,72 +285,99 @@
 	};
 	goHere();
 
-	// $("#myScrollspy").scrollspy({ offset: -75 });
+	$("#myScrollspy").scrollspy({ offset: -75 });
 
 
 
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
+// var TxtRotate = function(el, toRotate, period) {
+//   this.toRotate = toRotate;
+//   this.el = el;
+//   this.loopNum = 0;
+//   this.period = parseInt(period, 10) || 2000;
+//   this.txt = '';
+//   this.tick();
+//   this.isDeleting = false;
+// };
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+// TxtRotate.prototype.tick = function() {
+//   var i = this.loopNum % this.toRotate.length;
+//   var fullTxt = this.toRotate[i];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+//   if (this.isDeleting) {
+//     this.txt = fullTxt.substring(0, this.txt.length - 1);
+//   } else {
+//     this.txt = fullTxt.substring(0, this.txt.length + 1);
+//   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+//   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-  var that = this;
-  var delta = 300 - Math.random() * 100;
+//   var that = this;
+//   var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+//   if (this.isDeleting) { delta /= 2; }
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+//   if (!this.isDeleting && this.txt === fullTxt) {
+//     delta = this.period;
+//     this.isDeleting = true;
+//   } else if (this.isDeleting && this.txt === '') {
+//     this.isDeleting = false;
+//     this.loopNum++;
+//     delta = 500;
+//   }
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
+//   setTimeout(function() {
+//     that.tick();
+//   }, delta);
+// };
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
-};
+// window.onload = function() {
+//   var elements = document.getElementsByClassName('txt-rotate');
+//   for (var i=0; i<elements.length; i++) {
+//     var toRotate = elements[i].getAttribute('data-rotate');
+//     var period = elements[i].getAttribute('data-period');
+//     if (toRotate) {
+//       new TxtRotate(elements[i], JSON.parse(toRotate), period);
+//     }
+//   }
+//   // INJECT CSS
+//   var css = document.createElement("style");
+//   css.type = "text/css";
+//   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+//   document.body.appendChild(css);
+// };
 
 
 })(jQuery);
 
 
+function waveEffect(element, interval = 500) {
+      let text = element.textContent;
+      element.innerHTML = ""; // 기존 텍스트 삭제 후 개별 문자로 변환
 
+      // 글자마다 <span> 태그로 감싸기
+      text.split("").forEach((char, index) => {
+        let span = document.createElement("span");
+        span.textContent = char === " " ? "\u00A0" : char; // 공백 처리
+        element.appendChild(span);
+
+        // 애니메이션 효과 적용
+        setTimeout(() => {
+          span.style.opacity = "0";
+          span.style.transform = "translateY(10px)";
+        }, index * interval);
+
+        setTimeout(() => {
+          span.style.opacity = "1";
+          span.style.transform = "translateY(0)";
+        }, (index * interval) + 1000); // 1초 후 다시 나타남
+      });
+
+      // 반복 실행 (무한 루프)
+      setTimeout(() => waveEffect(element, interval), text.length * interval + 1000);
+    }
+
+    let waveText = document.getElementById("wave-text");
+    waveEffect(waveText, 500); // 0.5초 간격 실행
 
 
 
